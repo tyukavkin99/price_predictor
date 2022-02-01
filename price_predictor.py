2874be6fcc0b4cb3e3fc16ff69e0dc40ac80d860
tyukavkin99 <tyukavkin99@gmail.com>
2022-02-01 23:33:08 +0700

    Create price_predictor.py

---
+++ price_predictor.py
@@ -0,0 +1,46 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import streamlit as st 
+import datetime
+
+
+st.title("Simple stock predictor")
+st.sidebar.header("User inputs")
+
+
+try:
+    def user_input():
+        ticker = st.sidebar.text_input(label="Enter the indicator for company: ",
+                               value='')
+        period_options = ["1d","5d","1mo","3mo","6mo","1y","2y","5y","10y","ytd","max"]
+        period = st.sidebar.select_slider(label="Enter the period",options=period_options)
+        data = {
+            "Stock":ticker,
+            "Period":period
+        }
+        features = pd.DataFrame(data, index=[0])
+        return features
+    df = user_input()
+    st.write("Selected indicators")
+    st.write(df)
+    
+    st.write("Download the data")
+    
+    for i in df["Stock"]:
+        stock = i
+    for i in df["Period"]:
+        period = i
+ 
+    
+    ticker = yf.download(stock, period=period)
+    stock = pd.DataFrame(ticker)
+    
+    st.write(stock)
+    
+    
+except ValueError:
+    print("The indicator is the 4 letter code of company")
+
+
